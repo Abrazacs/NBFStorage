95353f9be5f944dcceb829190e15d28c978ca5fb
Abrazacs <sergeysemenov87@gmail.com>
2021-12-28 05:20:54 +0300

    123

--- src/main/java/server/AuthorizationService.java
+++ src/main/java/server/AuthorizationService.java
@@ -14,12 +14,14 @@ public class AuthorizationService {
     private ResultSet rSet;
 
     public void start() throws SQLException {
-        this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/archive_users","Abrazacs","12_qswd_12" );
+        this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/archive_users","root","12_qswd_12" );
         this.statement =  connection.createStatement();
+        System.out.println("DB connected");
 
     }
 
    boolean isUserAuthorized (User user) throws SQLException{
+       System.out.println("checking auth");
         pStatement = connection.prepareStatement("SELECT * FROM users WHERE login = ? AND password = ?");
         pStatement.setString(1, user.getUserName());
         pStatement.setString(2, user.getUserPassword());
